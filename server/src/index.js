e20e33e61ae814c67c0a3b85b8d4b7f44bad0086
maarvins <maarvins.19@gmail.com>
2022-03-01 17:35:40 -0300

    initial post configs on a endpoin register

--- server/src/index.js
+++ server/src/index.js
@@ -5,6 +5,15 @@ const cors = require("cors")
 app.use(cors())
 app.use(express.json())
 
+app.post("/register", (req,res) => {
+    const {name} = req.body
+    const {year} = req.body
+    const {seasons} = req.body
+    const {synopsis} = req.body
+    const {category} = req.body
+    
+})
+
 app.listen(3001, () => {
     console.log("Rodando servidor na porta 3001")
 })

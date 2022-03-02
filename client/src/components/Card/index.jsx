761c01b1cdf4898854e2bd1b21ae49d636107874
maarvins <maarvins.19@gmail.com>
2022-03-02 19:31:50 -0300

    trying consume the response from api data

--- client/src/components/Card/index.jsx
+++ client/src/components/Card/index.jsx
@@ -1,13 +1,13 @@
 import './card.scss'
 
-export function Card(){
+export function Card(props){
     return(
         <div className="card-container">
-            <h1 className="card-title">Titulo</h1>
-            <p className="card-years">Ano da serie</p>
-            <p className="card-seasons">Temporadas</p>
-            <p className="card-synopsis">Sinopse</p>
-            <p className="card-category">Categoria</p>
+            <h1 className="card-title">{props.name}</h1>
+            <p className="card-years">{props.years}</p>
+            <p className="card-seasons">{props.seasons}</p>
+            <p className="card-synopsis">{props.synopsis}</p>
+            <p className="card-category">{props.category}</p>
         </div>
     )
 }

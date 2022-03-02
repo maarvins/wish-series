beb3b1ae3750520c640cf38710c18bf552b02d9d
maarvins <maarvins.19@gmail.com>
2022-03-02 19:32:57 -0300

    breaking my head to make this data happen

--- server/src/db.js
+++ server/src/db.js
@@ -9,16 +9,8 @@ var conn = mysql.createConnection({
     database: process.env.DATABASE
 })
 
-conn.connect(function(err) {
-    if (err) return console.error("err: ", err)
-
-    var sql1 = "INSERT INTO categories (category) VALUES ('Drama')";
-    var sql2 = "SELECT * FROM categories";
-    
-    conn.query(sql2, function (err, result, fields) {
-        if (err) return console.error('err: ', err)
-
-        console.log("data: ", result)
-    })
-
+conn.connect(function (err) {
+    if (err) throw err
 })
+
+module.exports = conn
--- server/src/index.js
+++ server/src/index.js
@@ -5,9 +5,62 @@ const cors = require("cors")
 app.use(cors())
 app.use(express.json())
 
-app.post("/register", (req,res) => {
-    const {name, year, seasons, synopsis, category} = req.body    
+const conn = require('./db.js')
+
+app.get("/get_name", (_, res) => {
+    let sql = "SELECT * FROM series";
+
+    conn.query(sql, function (err, result) {
+        if (err) throw err;
+        res.json(JSON.parse(JSON.stringify(result)))
+    });
+    conn.end();
 })
+
+app.get("/get_years", (_, res) => {
+    let sql = "SELECT * FROM years";
+
+    conn.query(sql, function (err, result) {
+        if (err) throw err;
+        res.json(JSON.parse(JSON.stringify(result)))
+    });
+    conn.end();
+})
+
+app.get("/get_categories", (_, res) => {
+    let sql = "SELECT * FROM categories";
+
+    conn.query(sql, function (err, result) {
+        if (err) throw err;
+        res.json(JSON.parse(JSON.stringify(result)))
+    });
+    conn.end();
+})
+
+app.get("/getCards", (req, res) => {
+
+    let SQL = "SELECT * from series"
+
+    conn.query(SQL, (err, result) => {
+        if (err) console.log(err)
+        else JSON.parse(result)
+    })
+})
+
+app.post("/register", (req, res) => {
+    const { name, fk_year, seasons, synopse, fk_category } = req.body
+
+    var sql = "INSERT INTO series (name, fk_year, seasons, synopse, fk_category) VALUES (?,?,?,?,?)";
+
+    conn.query(sql, [name, fk_year, seasons, synopse, fk_category], (err, result) => {
+        if (err) console.log(err)
+        console.log(result)
+        res.send(result)
+    })
+
+    conn.end();
+})
+
 
 app.listen(3001, () => {
     console.log("Rodando servidor na porta 3001")

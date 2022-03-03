57f29c50c231a43e5f136e639c91df85695cd7f4
maarvins <maarvins.19@gmail.com>
2022-03-02 22:11:12 -0300

    add description on the card info

--- client/src/components/Card/index.jsx
+++ client/src/components/Card/index.jsx
@@ -3,11 +3,11 @@ import './card.scss'
 export function Card(props){
     return(
         <div className="card-container">
-            <h1 className="card-title">{props.name}</h1>
-            <p className="card-years">{props.years}</p>
-            <p className="card-seasons">{props.seasons}</p>
-            <p className="card-synopsis">{props.synopsis}</p>
-            <p className="card-category">{props.category}</p>
+            <h1 className="card-title">Título:{props.name}</h1>
+            <p className="card-years">Ano de Lançamento:{props.year}</p>
+            <p className="card-seasons">Temporadas: {props.seasons}</p>
+            <p className="card-synopsis">Sinopse: {props.synopse}</p>
+            <p className="card-category">Categoria: {props.category}</p>
         </div>
     )
 }
